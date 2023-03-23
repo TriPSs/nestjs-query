5b1caa9f5a66e77a487884a44f6ba03d1722a567
Tycho Bokdam <tycho@palmtreecoding.com>
2023-03-23 17:22:48 +0100

    chore: Fix linting of core

--- packages/core/src/interfaces/find-by-id-options.interface.ts
+++ packages/core/src/interfaces/find-by-id-options.interface.ts
@@ -1,6 +1,5 @@
 import { Filterable } from './filterable.interface'
-import { WithDeleted } from './with-deleted.interface'
 import { SelectRelations } from './select-relation.interface'
+import { WithDeleted } from './with-deleted.interface'
 
-export interface FindByIdOptions<DTO> extends Filterable<DTO>, SelectRelations<DTO>, WithDeleted {
-}
+export interface FindByIdOptions<DTO> extends Filterable<DTO>, SelectRelations<DTO>, WithDeleted {}
