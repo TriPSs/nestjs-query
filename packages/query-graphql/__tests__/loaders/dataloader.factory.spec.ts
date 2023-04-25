1cb465ea5c0056bd36faf1f5fe5515ba77a0335c
Silas Rosenkranz <s.rosenkranz@aura-tools.de>
2023-04-25 08:25:59 +0000

    add: unit-test for overriding the dataloader default implementation

--- packages/query-graphql/__tests__/loaders/dataloader.factory.spec.ts
+++ packages/query-graphql/__tests__/loaders/dataloader.factory.spec.ts
@@ -3,7 +3,15 @@ import DataLoader from 'dataloader'
 
 import { DataLoaderFactory } from '../../src/loader'
 
+jest.mock('dataloader')
+
 describe('DataLoaderFactory', () => {
+  const DataLoaderMock = DataLoader as jest.MockedClass<typeof DataLoader>
+
+  beforeEach(() => {
+    DataLoaderMock.mockClear()
+  })
+
   describe('getOrCreateLoader', () => {
     const createContext = (): ExecutionContext => ({} as unknown as ExecutionContext)
     const dataloadFn = (args: ReadonlyArray<string>): Promise<string[]> => Promise.resolve([...args])
@@ -11,7 +19,7 @@ describe('DataLoaderFactory', () => {
     it('should create a dataloader and add it to the context', () => {
       const context = createContext()
       const loader = DataLoaderFactory.getOrCreateLoader(context, 'loader', () => dataloadFn)
-      expect(loader).toBeInstanceOf(DataLoader)
+      expect(loader).toBeInstanceOf(DataLoaderMock)
     })
 
     it('should return the same dataloader if called twice', () => {
@@ -19,6 +27,16 @@ describe('DataLoaderFactory', () => {
       const loader = DataLoaderFactory.getOrCreateLoader(context, 'loader', () => dataloadFn)
       const loader2 = DataLoaderFactory.getOrCreateLoader(context, 'loader', () => dataloadFn)
       expect(loader).toBe(loader2)
+    })
+
+    it('should override the default dataloader implementation if a custom one is provided', () => {
+      const context = createContext()
+      const batchScheduleFn = jest.fn()
+
+      DataLoaderFactory.getOrCreateLoader(context, 'loader', () => dataloadFn, { batchScheduleFn })
+
+      expect(DataLoaderMock).toHaveBeenCalledTimes(1)
+      expect(DataLoaderMock).toHaveBeenCalledWith(dataloadFn, expect.objectContaining({ batchScheduleFn }))
     })
   })
 })
