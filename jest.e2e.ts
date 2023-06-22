8e57333b33bb11d7150c9e4001737b4379b49e71
Tycho Bokdam <tycho@palmtreecoding.com>
2023-06-22 20:54:27 +0200

    chore: Updated NX

--- jest.e2e.ts
+++ jest.e2e.ts
@@ -2,14 +2,15 @@
 export default {
   displayName: 'examples',
   preset: './jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: process.cwd() + '/examples/tsconfig.spec.json'
-    }
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest'
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: process.cwd() + '/examples/tsconfig.spec.json'
+      }
+    ]
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   testMatch: ['**/examples/**/e2e/**/*.spec.ts'],
