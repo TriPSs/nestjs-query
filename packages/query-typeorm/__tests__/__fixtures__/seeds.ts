af4df026175b2a2bd5c67e0d1fc4e6b7e83cdf72
rezoled <peled@rezonate.io>
2024-11-26 11:31:10 +0200

    fix all

--- packages/query-typeorm/__tests__/__fixtures__/seeds.ts
+++ packages/query-typeorm/__tests__/__fixtures__/seeds.ts
@@ -13,7 +13,7 @@ export const TEST_ENTITIES: TestEntity[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((
 	return {
 		id,
 		boolType: i % 2 === 0,
-		dateType: new Date(`2020-02-${i} 12:00`),
+		dateType: new Date(`2020-02-${i}T12:00:00:00.000Z`),
 		numberType: i,
 		stringType: `foo${i}`,
 	} as TestEntity;
