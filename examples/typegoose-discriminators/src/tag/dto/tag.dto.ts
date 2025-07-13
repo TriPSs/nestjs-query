fbe82836bafed622414959701524efa628a98e9c
Scott Molinari <scott.molinari@m8a.io>
2025-07-13 08:31:38 +0200

    tests: copy over typegoose to typegoose-discriminators

---
+++ examples/typegoose-discriminators/src/tag/dto/tag.dto.ts
@@ -0,0 +1,66 @@
+/* eslint-disable no-param-reassign */
+import { GraphQLISODateTime, ID, ObjectType } from '@nestjs/graphql'
+import {
+  BeforeCreateMany,
+  BeforeCreateOne,
+  BeforeUpdateMany,
+  BeforeUpdateOne,
+  CreateManyInputType,
+  CreateOneInputType,
+  CursorConnection,
+  FilterableField,
+  KeySet,
+  ObjectId,
+  QueryOptions,
+  UpdateManyInputType,
+  UpdateOneInputType
+} from '@ptc-org/nestjs-query-graphql'
+import mongoose from 'mongoose'
+
+import { GqlContext } from '../../auth.guard'
+import { getUserName } from '../../helpers'
+import { TodoItemDTO } from '../../todo-item/dto/todo-item.dto'
+
+@ObjectType('Tag')
+@KeySet(['id'])
+@QueryOptions({ enableTotalCount: true })
+@CursorConnection('todoItems', () => TodoItemDTO)
+@BeforeCreateOne((input: CreateOneInputType<TagDTO>, context: GqlContext) => {
+  input.input.createdBy = getUserName(context)
+  return input
+})
+@BeforeCreateMany((input: CreateManyInputType<TagDTO>, context: GqlContext) => {
+  const createdBy = getUserName(context)
+  input.input = input.input.map((c) => ({ ...c, createdBy }))
+  return input
+})
+@BeforeUpdateOne((input: UpdateOneInputType<TagDTO>, context: GqlContext) => {
+  input.update.updatedBy = getUserName(context)
+  return input
+})
+@BeforeUpdateMany((input: UpdateManyInputType<TagDTO, TagDTO>, context: GqlContext) => {
+  input.update.updatedBy = getUserName(context)
+  return input
+})
+export class TagDTO {
+  @ObjectId()
+  _id: mongoose.Types.ObjectId
+
+  @FilterableField(() => ID)
+  id!: string
+
+  @FilterableField()
+  name!: string
+
+  @FilterableField(() => GraphQLISODateTime)
+  createdAt!: Date
+
+  @FilterableField(() => GraphQLISODateTime)
+  updatedAt!: Date
+
+  @FilterableField({ nullable: true })
+  createdBy?: string
+
+  @FilterableField({ nullable: true })
+  updatedBy?: string
+}
---
+++ examples/typegoose-discriminators/src/todo-item/dto/todo-item.dto.ts
@@ -0,0 +1,54 @@
+import { Field, GraphQLISODateTime, ID, ObjectType } from '@nestjs/graphql'
+import { CursorConnection, FilterableField, KeySet, ObjectId, QueryOptions } from '@ptc-org/nestjs-query-graphql'
+import mongoose from 'mongoose'
+
+import { AuthGuard } from '../../auth.guard'
+import { SubTaskDTO } from '../../sub-task/dto/sub-task.dto'
+import { TagDTO } from '../../tag/dto/tag.dto'
+
+@ObjectType('TodoItem')
+@KeySet(['id'])
+@QueryOptions({ enableTotalCount: true })
+@CursorConnection('subTasks', () => SubTaskDTO, {
+  update: { enabled: true },
+  guards: [AuthGuard]
+})
+@CursorConnection('tags', () => TagDTO, {
+  update: { enabled: true },
+  remove: { enabled: true },
+  guards: [AuthGuard]
+})
+export class TodoItemDTO {
+  @ObjectId()
+  _id: mongoose.Types.ObjectId
+
+  @FilterableField(() => ID)
+  id!: string
+
+  @FilterableField()
+  title!: string
+
+  @FilterableField({ nullable: true })
+  description?: string
+
+  @FilterableField()
+  completed!: boolean
+
+  @FilterableField(() => GraphQLISODateTime)
+  createdAt!: Date
+
+  @FilterableField(() => GraphQLISODateTime)
+  updatedAt!: Date
+
+  @Field()
+  age!: number
+
+  @FilterableField()
+  priority!: number
+
+  @FilterableField({ nullable: true })
+  createdBy?: string
+
+  @FilterableField({ nullable: true })
+  updatedBy?: string
+}
