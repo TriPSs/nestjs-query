e7d5011bc68e2558a99568b29519221489914931
Tycho Bokdam <tycho@palmtreecoding.com>
2026-08-26 18:03:44 +0200

    fix(examples): Resolve lint warnings

--- examples/basic-rest/src/tag/tag.entity.ts
+++ examples/basic-rest/src/tag/tag.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, ManyToMany, ObjectType, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import { Column, CreateDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
 
 import { TodoItemEntity } from '../todo-item/todo-item.entity'
 
